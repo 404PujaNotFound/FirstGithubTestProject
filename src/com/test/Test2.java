4b9c269b6b7271bf403da6e934a5f07e7d8c072b
PUJA <PUJA@DESKTOP-3S0ALQA>
2025-07-26 12:11:30 +0530

    commit again

--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -1,7 +1,7 @@
 package com.test;
 
-public class Test1 {
+public class Test2 {
 	public static void main(String args[]) {
-		System.out.println("hello puja aaaa");
+		System.out.println("hello puja");
 }
 }
--- test2.java
+++
@@ -1,6 +0,0 @@
-
-public class test2 {
-	public static void main() {
-		System.out.println("hello puja mandal");
-}
-}
